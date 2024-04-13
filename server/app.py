3c09d1040ca74037606f516f33c66fd6ae39e686
Priscilla-22 <priscillamw123@gmail.com>
2024-04-13 13:57:38 +0300

    feat:Password Protection

--- server/app.py
+++ server/app.py
@@ -6,17 +6,18 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User
 
+
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
+
 class Signup(Resource):
-    
+
     def post(self):
         json = request.get_json()
         user = User(
@@ -27,17 +28,47 @@ class Signup(Resource):
         db.session.commit()
         return user.to_dict(), 201
 
+
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id is not None:
+            user = User.query.get(user_id)
+            if user is not None:
+                return user.to_dict(), 200
+        return {}, 204
+
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        if not username or not password:
+            return {}, 400
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if 'user_id' in session:
+            session.clear()
+            return {}, 204
+        else:
+            return {}, 400
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
